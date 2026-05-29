133c101f0f08336412669c74d3a864cbbebdb554
Hao He <haohe90@gmail.com>
2026-05-29 16:22:04 +0800

    update toml

--- src/microVis/__init__.py
+++ src/microVis/__init__.py
@@ -1,6 +1,7 @@
 """microVis -- interactive visualization for microProfiler microscopy datasets."""
+
 from __future__ import annotations
 
-__version__ = "0.2.0"
+__version__ = "0.4.0"
 
 __all__ = ["__version__"]
